fe4e1de902038e939b6788d47ff7abea08aed581
Abdulaleam <167490133+Abdulaleam@users.noreply.github.com>
2026-06-21 23:59:02 +0300

    ADDED THE ABILITY IM TRYING DO SMTH LIKE USING THE START ITEM WILL ADD THE PLAYER TO THE ACTIVE GAME, AND THE STOP WILL REMOVE !

---
+++ src/main/java/rainy/net/lava/LavaStartAbility.java
@@ -0,0 +1,57 @@
+package rainy.net.lava;
+
+import net.minecraft.block.Blocks;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+public class LavaStartAbility {
+
+    private static final Set<UUID> ACTIVE = new HashSet<>();
+    private static int ticks = 0;
+
+    public static void toggleStart(ServerPlayerEntity player) {
+        ACTIVE.add(player.getUuid());}
+
+    public static void toggleStop(ServerPlayerEntity player) {
+        ACTIVE.remove(player.getUuid());}
+
+    public static boolean isActive(ServerPlayerEntity player) {
+        return ACTIVE.contains(player.getUuid());}
+
+    public static void tick(ServerPlayerEntity player, World world) {
+        if (!isActive(player))
+        {return;}
+        ticks++;
+        BlockPos center = player.getBlockPos();
+
+        if (ticks % 30 == 0) {
+
+            int x = center.getX() + (int)(Math.random() * 18) - 9;
+            int z = center.getZ() + (int)(Math.random() * 18) - 9;
+
+            for (int y = 255; y >= 0; y--) {
+
+                BlockPos pos = new BlockPos(x, y, z);
+                if (!world.getBlockState(pos).isAir()) {
+              world.setBlockState(pos, Blocks.AIR.getDefaultState());
+                }}}
+        if (ticks % 10 == 0) {
+
+            int x = center.getX() + (int)(Math.random() * 18) - 9;
+            int z = center.getZ() + (int)(Math.random() * 18) - 9;
+
+            BlockPos pos = new BlockPos(x, center.getY(), z);
+            if (world.getBlockState(pos).isAir()) {
+                world.setBlockState(pos, Blocks.LAVA.getDefaultState());
+            }
+        }
+
+        if (ticks >= 100000)
+        {ticks = 0;}
+    }
+}
